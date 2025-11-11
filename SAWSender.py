48b917bca386db6c4df101c942c0fa9d91fcf1e2
anaylo5450 <167348202+anaylo5450@users.noreply.github.com>
2025-11-10 23:37:03 -0500

    basic SAW sender
    Implement sender for Stop and Wait protocol using UDP. Simple python program that allows the user to specify the IP and port of the receiver. receiver must be on the same network, must be improved according to the books specification

--- SAWSender.py
+++ SAWSender.py
@@ -1,1 +1,24 @@
+#sender for Stop and Wait procedure
+import socket, time
 
+receiver_ip = input("Enter receiver IP: ")  
+receiver_port = input("ender receiver port: ")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(1)  # 1 second timeout
+
+message = input("Enter message to send: ")
+data = message.encode()
+
+while True:
+    sock.sendto(data, (receiver_ip, receiver_port))
+    print("Sent:", message)
+    try:
+        ack, _ = sock.recvfrom(1024)
+        if ack == b"ACK":
+            print("ACK received! Transmission complete.")
+            break
+    except socket.timeout:
+        print("Timeout, resending...")
+
+sock.sendto(b"END", (receiver_ip, receiver_port))
